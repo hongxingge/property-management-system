f29d61075a33a6a70079531d9c6aeca29d00a1f0
Administrator <1526738077@qq.com>
2026-09-10 19:25:45 +0800

    feat: 优化业主端公告详情返回交互

--- management-vue/src/components/ClientNewsInfo.vue
+++ management-vue/src/components/ClientNewsInfo.vue
@@ -1,8 +1,24 @@
 <template>
-  <div style="padding: 20px;width: 100%">
-    <h2 style="text-align: center">{{ currentNews.title }}</h2>
-    <div style="width: 80%;margin:20px auto;" v-html="currentNews.content">
+  <div class="news-detail">
+    <!-- 返回按钮 -->
+    <div class="back-bar">
+      <el-button
+          type="text"
+          icon="el-icon-arrow-left"
+          @click="goBack"
+      >
+        返回公告列表
+      </el-button>
+    </div>
 
+    <!-- 公告内容 -->
+    <div class="news-content">
+      <h2>{{ currentNews.title }}</h2>
+
+      <div
+          class="content"
+          v-html="currentNews.content">
+      </div>
     </div>
   </div>
 </template>
@@ -13,15 +29,16 @@ import {apiGetNewsById} from "@/utils/request";
 
 export default {
   mounted() {
-    apiGetNewsById({//通过主界面传过来的id获取具体的公告信息
-      'id': this.$route.query.id
+    apiGetNewsById({
+      // 通过主界面传过来的 id 获取具体的公告信息
+      id: this.$route.query.id
     }).then(res => {
       this.currentNews = res
-      this.showNewsDialog = true
     }).catch(err => {
       this.$message.error(err);
     })
   },
+
   data() {
     return {
       currentNews: {
@@ -31,11 +48,43 @@ export default {
       }
     }
   },
-  methods:{
+
+  methods: {
+    // 返回公告列表
+    goBack() {
+      this.$router.back()
+    }
   }
 }
 </script>
 
 <style scoped>
+.news-detail {
+  width: 100%;
+  min-height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
 
+.back-bar {
+  width: 80%;
+  margin: 0 auto;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #eeeeee;
+}
+
+.news-content {
+  width: 80%;
+  margin: 20px auto;
+}
+
+.news-content h2 {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.content {
+  line-height: 1.8;
+  font-size: 16px;
+}
 </style>
